39780f13db2759b416ed35d3b271e1c094de7cfa
felipe Januario <felipejanuario86@gmail.com>
2020-06-11 19:10:24 -0300

    criando lista de nomes

---
+++ src/com/digitalinnovationone/list/ExempleList.java
@@ -0,0 +1,18 @@
+package com.digitalinnovationone.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExempleList {
+
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Carlos");
+        nomes.add("Pedro");
+        nomes.add("Juliana");
+        nomes.add("João");
+
+        System.out.println(nomes);
+    }
+}
